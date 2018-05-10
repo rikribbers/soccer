211e77785464b54ac5ebab02d848138e18dd1442
r.ribbers <rik.ribbers@isatis-health.nl>
2018-05-10 14:50:04 +0200

    proxy in prod

--- Poule/Startup.cs
+++ Poule/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -52,6 +53,14 @@ namespace Poule
                 app.UseDeveloperExceptionPage();
             }
 
+            if (_env.IsProduction())
+            {
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+
+            }
             app.UseStaticFiles();
             app.UseMvc(ConfigureRoutes);
             
